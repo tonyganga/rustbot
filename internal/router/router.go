9853f14b9c73b295586f3f0f4ae327336be290a7
Tony Ganga <anthonyganga@gmail.com>
2022-02-25 19:45:12 -0500

    lint

--- internal/router/router.go
+++ internal/router/router.go
@@ -30,17 +30,23 @@ func RustHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		p.Add(ReactionMessage(), InfoMessage())
 		p.SetPageFooters()
 
-		p.Widget.Handle(UPWARD_TREND_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
+		err := p.Widget.Handle(UPWARD_TREND_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
 			ids := battlemetrics.GetListOfRustServers("")
 			msg, err := s.ChannelMessageSend(m.ChannelID, battlemetrics.GetRankedListOfRustServers(ids))
 			if err != nil {
 				log.Print(err)
 			}
 			time.Sleep(time.Second * 15)
-			s.ChannelMessageDelete(m.ChannelID, msg.ID)
+			err = s.ChannelMessageDelete(m.ChannelID, msg.ID)
+			if err != nil {
+				log.Println(err)
+			}
 		})
+		if err != nil {
+			log.Println(err)
+		}
 
-		p.Widget.Handle(ID_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
+		err = p.Widget.Handle(ID_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
 			if msg, err := w.QueryInput("What is the ID of the Rust server you are looking for?", r.UserID, 15*time.Second); err == nil {
 				id := msg.Content
 				match, err := regexp.MatchString(`^[0-9]+$`, id)
@@ -56,12 +62,21 @@ func RustHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				}
 				server := battlemetrics.GetRustServer(id)
 				p.Add(server.RustServerMessage())
-				p.Goto(len(p.Pages) - 1)
-				p.Update()
+				err = p.Goto(len(p.Pages) - 1)
+				if err != nil {
+					log.Println(err)
+				}
+				err = p.Update()
+				if err != nil {
+					log.Println(err)
+				}
 			}
 		})
+		if err != nil {
+			log.Println(err)
+		}
 
-		p.Widget.Handle(SEARCH_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
+		err = p.Widget.Handle(SEARCH_EMOJI, func(w *dgwidgets.Widget, r *discordgo.MessageReaction) {
 			if msg, err := w.QueryInput("What Rust server are you looking for?", r.UserID, 15*time.Second); err == nil {
 				var query string
 				content := msg.Content
@@ -75,11 +90,20 @@ func RustHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 					log.Print(err)
 				}
 				time.Sleep(time.Second * 15)
-				s.ChannelMessageDelete(m.ChannelID, msg.ID)
+				err = s.ChannelMessageDelete(m.ChannelID, msg.ID)
+				if err != nil {
+					log.Println(err)
+				}
 			}
 		})
+		if err != nil {
+			log.Println(err)
+		}
 
-		p.Spawn()
+		err = p.Spawn()
+		if err != nil {
+			log.Println(err)
+		}
 		return
 	}
 }
